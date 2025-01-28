78ff985e107b3b1fcb46eb22fbe8b184a3c0eed2
fathiasafio <nafisoadanfarah@gmail.com>
2025-01-28 19:48:09 +0300

    Add files via upload

---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+
+    echo "Name: $name<br>";
+    echo "Email: $email<br>";
+}
+?>
